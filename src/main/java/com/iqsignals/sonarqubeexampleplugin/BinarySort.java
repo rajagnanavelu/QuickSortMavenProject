9b0107e3890da6aa40446a552e6a8bb5472f00bc
rajagnanavelu <raja.gnanavelu@atos.net>
2020-08-19 16:00:58 +0530

    Initial commit

---
+++ src/main/java/com/iqsignals/sonarqubeexampleplugin/BinarySort.java
@@ -0,0 +1,9 @@
+package com.iqsignals.sonarqubeexampleplugin;
+
+public class BinarySort {
+
+    public static void main(String []param) {
+        System.out.println("Main method is invoked");
+    }
+
+}
